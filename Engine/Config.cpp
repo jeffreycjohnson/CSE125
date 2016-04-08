1df200a11d7f431409e859dc2f736f7a73e4383d
Dexter Friedman <drfriedm@ucsd.edu>
2016-04-07 17:39:14 -0700

    Implementing Config.cpp, (untested, not sure if compiles)

--- Engine/Config.cpp
+++ Engine/Config.cpp
@@ -1,15 +1,118 @@
 #include "Config.h"
+#include <iostream>
+#include <fstream>
+#include <sstream>
 
-
-Config::Config(std::string& configPath)
+Config::Config(std::string& configFilePath)
 {
-	if (instance == nullptr) {
-		instance = this;
-		configFilePath = configPath;
-	}
+	this->configFilePath = configFilePath;
+	load();
 }
 
 
 Config::~Config()
 {
+}
+
+
+void Config::load() {
+	// Open the config file
+	std::ifstream file(configFilePath);
+	std::string line;
+	std::string currentSection;
+	long lineNumber = 0;
+
+	while (file.good()) {
+		getline(file, line);
+		lineNumber++;
+		auto type = TokenizedStringType::UNKNOWN;
+
+		// Tokenize will figure out what kind of line this
+		// is, (comment, section header, or key-value pair)
+		auto tokens = tokenize(line, type);
+
+		switch (type) {
+			case TokenizedStringType::COMMENT: {
+				continue;
+			} 
+			break;
+			case TokenizedStringType::SECTION: {
+				// tokens list is '[', 'Name', ']'
+				currentSection = tokens[1];
+				if ( !hasSection(currentSection) ) {
+					ConfigSection c;
+					sections[currentSection] = c; // just copy. avoid ptrs.
+				}
+			}
+			break;
+			case TokenizedStringType::KEY_VALUE_PAIR: {
+				// tokens list is '{{key}}', '=', '{{value}}'
+				sections[currentSection].set(tokens[0], tokens[2]);
+			}
+			break;
+			case TokenizedStringType::UNKNOWN: {
+				std::cerr << "Error parsing: {" << configFilePath << "} at line (" << lineNumber << ")" << std::endl;
+				break;
+			}
+			break;
+		}
+	}
+
+	file.close();
+
+}
+
+bool Config::hasSection(std::string& section) {
+	// If the section name doesn't exist in our map, return false
+	auto iter = sections.find(section);
+	return ( iter != sections.end() );
+}
+
+/**
+ * This function returns a vector of strings, where every string is a valid
+ * token in the INI file. Since INI files have different kinds of lines, this
+ * function returns different tokens depending on the line we read from the file.
+ * This is what the "type" var is for. It let's us know what to expect from the
+ * return value:
+ *   a COMMENT, a SECTION header, or a KEY_VALUE_PAIR
+ *
+ * NOTE:  If we were unable to determine
+ */
+std::vector<std::string> Config::tokenize(const std::string& line, ConfigFile::TokenizedStringType type) {
+
+	std::vector<std::string> tokens;
+
+	if ( line.length() == 0 ) {
+		type = TokenizedStringType::COMMENT;
+		return tokens;
+	}
+
+	// Detect what kind of line this is, by peeking at the first char
+	if ( line.at(0) == TOK_L_BRAC ) {
+
+
+		return tokens;
+	}
+	else if ( line.at(0) == TOK_COMMENT ) {
+		return tokens;
+	}
+	else {
+		auto found = line.find(TOK_EQUALS);
+
+		// We found an equals character. Hooray!
+		if ( found != std::string::npos ) {
+			std::string key   = line.substr(0, found);
+			std::string value = line.substr(found + 1, line.length() - found - 1);
+			tokens.push_back(key);
+			tokens.push_back(TOK_EQUALS);
+			tokens.push_back(value);
+			type = TokenizedStringType::KEY_VALUE_PAIR;
+		}
+		else {
+			type = TokenizedStringType::UNKNOWN;
+		}
+	}
+
+	return tokens;
+
 }
--- Engine/Config.h
+++ Engine/Config.h
@@ -7,14 +7,13 @@
 #define TOK_L_BRAC "["
 #define TOK_R_BRAC "]"
 #define TOK_EQUALS "="
+#define TOK_COMMENT ";"
 
 /*
  Defines a helper class for easily reading from a series of configuration files
  by key. We will be using .INI files, for most things. If we decide to have a
  particular format for texture/material files, we can also add to this class as
  needed.
-
- *** Please note that Config is a singleton! ***
 
  ABOUT INI FILES
  ---------------
@@ -34,31 +33,42 @@
 
  Once all of the config files have been loaded, reading them should be easy.
 
- Config::getInt("GameConfig","foo");    // Returns 123
+ ConfigFile::getInt("GameConfig","foo");    // Returns 123
 
  */
-class Config
+class ConfigFile
 {
 public:
-	Config(std::string& configPath);
-	~Config();
+
+	ConfigFile(std::string& configFilePath);
+	~ConfigFile();
 
 	// Retrieve a key from one of the many config files we've loaded
-	static int getInt(const std::string& section, const std::string& key);
-	static float getFloat(const std::string& section, const std::string& key);
-	static std::string& getString(const std::string& section, const std::string& key);
+	int getInt(const std::string& section, const std::string& key);
+	float getFloat(const std::string& section, const std::string& key);
+	std::string& getString(const std::string& section, const std::string& key);
 
 private:
-	static Config* instance;
+
+	enum TokenizedStringType {
+		COMMENT = 0x0,
+		SECTION = 0x1,
+		KEY_VALUE_PAIR = 0x2,
+		UNKNOWN = 0x3
+	}
 
 	// Member variables
 	std::string configFilePath;
+	std::map<std::string, ConfigSection> sections;
 
 	// Member functions
-	void loadConfigFiles();
+	void load();
+	bool hasSection(std::string& section);
+	std::vector<std::string> tokenize(const std::string& line, ConfigFile::TokenizedStringType type);
 
 	// A section of an .ini file
 	class ConfigSection {
+		friend ConfigFile;
 	public:
 		ConfigSection();
 		~ConfigSection();
@@ -67,25 +77,8 @@ private:
 		bool hasKey(const std::string& key);
 	
 	private:
+		void set(const std::string& key, const std::string& value);
 		std::map<std::string,std::string> keyValuePairs;
 	};
-
-	// An individual .ini file
-	class ConfigFile {
-	public:
-		ConfigFile(std::string& filename);
-		~ConfigFile();
-		std::string& get(std::string& section, std::string& key);
-
-	private:
-		bool hasSection(std::string& section);
-
-	private:
-		std::map<std::string, ConfigSection> sections;
-		std::vector<std::string> tokenize(std::string&);
-	};
-
-	// List of all the config files we've loaded for the game
-	std::vector<ConfigFile> files;
 
 };
